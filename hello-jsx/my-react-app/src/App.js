b3b7a84a29d7ddd8d7c0c3dccd0fafeca7b49814
mohamedmehdigara <garamehdi57@yahoo.com>
2023-09-09 14:45:26 +0100

    added files

---
+++ hello-jsx/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const age = 25;
+const name = 'John Doe';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+    </div>
+  );
+}
+
+export default App;
